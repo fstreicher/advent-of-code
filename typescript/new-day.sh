3018b7bc46df7626d93fcf1095402e0298ac1caa
Fabian Streicher <fabi.streicher@gmail.com>
2024-12-01 14:59:25 +0100

    feat(typescript): add template generator

---
+++ typescript/new-day.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if two arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <year> <day>"
+  exit 1
+fi
+
+# Assign input arguments to variables
+year=$1
+day=$2
+
+# Create the nested folder year/day
+mkdir -p "$year/$day"
+
+# Create an empty text file
+touch "$year/$day/input.txt"
+
+# Create a template typescript file
+cat <<EOL > "$year/$day/solution.ts"
+// https://adventofcode.com/$year/day/$day
+
+
+
+// Challenge 1
+export function challenge1(input: Array<string>) {
+
+  return;
+}
+
+
+// Challenge 2
+export function challenge2(input: Array<string>) {
+
+  return;
+}
+EOL
